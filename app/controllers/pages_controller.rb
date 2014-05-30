023a909325ddc31ab3450043f9f6255148a4342d
Forkk <forkk@forkk.net>
2014-05-30 12:41:24 -0500

    Add placeholder home page

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,6 @@
+# The PagesController is a controller for rendering static pages such as the
+# homepage and maybe an FAQ page.
+class PagesController < ApplicationController
+    def home
+    end
+end
