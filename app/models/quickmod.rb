002b5b0590d894d1a280c965d99e5ea88702e761
Forkk <forkk@forkk.net>
2014-06-04 14:43:42 -0500

    Fix UID validity error message.
    For future reference, max, "alphanumeric characters" includes numbers
    too.

--- app/models/quickmod.rb
+++ app/models/quickmod.rb
@@ -2,7 +2,7 @@ class QuickMod < ActiveRecord::Base
     extend FriendlyId
     friendly_id :uid
 
-    validates :uid, presence: true, uniqueness: true, format: {with: /\A[a-zA-Z0-9_\-]+\Z/, message: 'only alphanumeric characters, numbers dashes and underscores are allowed'}
+    validates :uid, presence: true, uniqueness: true, format: {with: /\A[a-zA-Z0-9_\-\.]+\Z/, message: 'only alphanumeric characters, dashes, dots, and underscores are allowed'}
     validates :name, presence: true
     validate :uid_reserved?
 
